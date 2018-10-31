4c6a6486c26c2e3525bb9f6cea38a09a590a6da8
Ivan Espinosa <ivandespinosa@gmail.com>
2018-10-31 10:04:40 -0500

    Include console project

---
+++ sln_parqueadero/ParkingConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic;
+using DataAccess.Contracts;
+using DataAccess.SqlServer;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace ParkingConsoleApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer unityContainer = new UnityContainer();
+
+            unityContainer.RegisterType<Context, ContextSqlServer>();
+            unityContainer.RegisterType<IDataAccessParking, DataAccessSqlServerParking>();
+
+            FacadaParking facadaParking = unityContainer.Resolve<FacadaParking>();
+            
+            var parking = facadaParking.GetParking();
+
+            Console.WriteLine("Ok " + parking.Name);
+            Console.ReadKey();
+        }
+    }
+}
